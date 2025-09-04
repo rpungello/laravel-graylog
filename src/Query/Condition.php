62ce98c27c71cbfa1fdd1ab834ed6bfb7d4cb431
Rob Pungello <rob@rpun.io>
2025-09-04 13:23:19 -0400

    Add regex and less/greater than operators

--- src/Query/Condition.php
+++ src/Query/Condition.php
@@ -2,6 +2,7 @@
 
 namespace Rpungello\Graylog\Query;
 
+use InvalidArgumentException;
 use Stringable;
 
 class Condition implements Stringable
@@ -12,8 +13,14 @@ class Condition implements Stringable
     {
         if ($this->field instanceof Builder) {
             return "$this->boolean ($this->field)";
-        } else {
+        } elseif ($this->operator === 'regex') {
+            return "$this->boolean $this->field:/$this->value/";
+        } elseif ($this->operator === '=') {
             return "$this->boolean $this->field:\"$this->value\"";
+        } elseif(in_array($this->operator, ['>', '>=', '<', '<='])) {
+            return "$this->boolean $this->field:$this->operator$this->value";
+        } else {
+            throw new InvalidArgumentException('Unsupported condition type');
         }
     }
 }
--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -58,3 +58,28 @@ it('can construct three-tier queries', function () {
         });
     expect((string)$query)->toBe('field1:"value1" && (field2:"value2" || (field3:"value3" && field4:"value4"))');
 });
+
+it('can construct basic regex queries', function () {
+    $query = Builder::begin()->and('field1', '\d+', 'regex');
+    expect((string)$query)->toBe('field1:/\d+/');
+});
+
+it('can construct basic greater than queries', function () {
+    $query = Builder::begin()->and('field1', 5, '>');
+    expect((string)$query)->toBe('field1:>5');
+});
+
+it('can construct basic greater than or equal queries', function () {
+    $query = Builder::begin()->and('field1', 5, '>=');
+    expect((string)$query)->toBe('field1:>=5');
+});
+
+it('can construct basic less than queries', function () {
+    $query = Builder::begin()->and('field1', 5, '<');
+    expect((string)$query)->toBe('field1:<5');
+});
+
+it('can construct basic less than or equal queries', function () {
+    $query = Builder::begin()->and('field1', 5, '<=');
+    expect((string)$query)->toBe('field1:<=5');
+});
