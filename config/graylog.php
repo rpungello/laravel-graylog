098b4f9c5b6a2bc72bdea649762b0690daa8e11b
Rob Pungello <rob@rpun.io>
2025-09-04 08:08:56 -0400

    Configure guzzle client

--- config/graylog.php
+++ config/graylog.php
@@ -1,7 +1,7 @@
 <?php
 
-// config for Rpungello/Graylog
 return [
+    'https' => env('GRAYLOG_API_HTTPS', false),
     'host' => env('GRAYLOG_API_HOST'),
     'port' => env('GRAYLOG_API_PORT', 9000),
     'token' => env('GRAYLOG_API_TOKEN'),
--- src/Graylog.php
+++ src/Graylog.php
@@ -2,4 +2,28 @@
 
 namespace Rpungello\Graylog;
 
-class Graylog {}
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Contracts\Foundation\Application;
+
+class Graylog
+{
+    protected Client $client;
+
+    public function __construct(Application $app)
+    {
+        $uri = tap(new Uri)
+            ->withScheme($app['config']->get('graylog.https') ? 'https' : 'http')
+            ->withHost($app['config']->get('graylog.host'))
+            ->withPort($app['config']->get('graylog.port'));
+
+        $this->client = new Client([
+            'base_uri' => $uri,
+            RequestOptions::AUTH => [
+                $app['config']->get('graylog.token'),
+                'token',
+            ]
+        ]);
+    }
+}
