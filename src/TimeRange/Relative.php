788932134c587a29ac44e874298d535af8388321
Rob Pungello <rob@pdfsystems.com>
2025-09-04 09:54:14 -0400

    Allow intervals for relative times

--- src/TimeRange/Relative.php
+++ src/TimeRange/Relative.php
@@ -2,15 +2,26 @@
 
 namespace Rpungello\Graylog\TimeRange;
 
+use Carbon\CarbonInterval;
+
 class Relative extends TimeRange
 {
-    public function __construct(protected int $numberOfSeconds) {}
+    public function __construct(protected int|CarbonInterval $interval) {}
 
     public function toArray(): array
     {
         return [
             'type' => 'relative',
-            'range' => $this->numberOfSeconds,
+            'range' => $this->getIntervalAsNumberOfSeconds(),
         ];
+    }
+
+    private function getIntervalAsNumberOfSeconds(): int
+    {
+        if (is_int($this->interval)) {
+            return $this->interval;
+        } else {
+            return $this->interval->totalSeconds;
+        }
     }
 }
