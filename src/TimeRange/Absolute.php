66e057c8eb9f2a959cde7e72863e173305306890
Rob Pungello <rob@pdfsystems.com>
2025-09-04 09:54:22 -0400

    Add absolute times

---
+++ src/TimeRange/Absolute.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rpungello\Graylog\TimeRange;
+
+use DateTimeInterface;
+
+class Absolute extends TimeRange
+{
+    public function __construct(protected DateTimeInterface $start, protected DateTimeInterface $end)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => 'absolute',
+            'from' => $this->start->format('c'),
+            'to' => $this->end->format('c'),
+        ];
+    }
+}
